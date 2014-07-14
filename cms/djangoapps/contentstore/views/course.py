87c3c213797c5d478c470eaacfb5ca182a95d648
Oleg Marshev <oleh.marshev@gmail.com>
2014-07-14 16:34:14 +0300

    Send course outline url.

--- cms/djangoapps/contentstore/views/course.py
+++ cms/djangoapps/contentstore/views/course.py
@@ -1009,7 +1009,7 @@ def group_configurations_list_handler(request, course_key_string):
         configurations = []
         for partition in course.user_partitions:
             configuration = partition.to_json()
-            configuration['usage'] = unit_urls.get(partition.id, [])
+            configuration['usage'] = unit_urls.get(partition.id, reverse_course_url('course_handler', course_key))
             configurations.append(configuration)
 
         return render_to_response('group_configurations.html', {
