9c05991bd357b54c40f247039845003873cd0f89
karthik <karthikbangera87@gmail.com>
2014-07-30 20:41:46 +0000

    adding an additional course and verifying that the appropriate badge is rendered for this course in terms of issuer and program in the progress template

--- lms/djangoapps/badges/utils.py
+++ lms/djangoapps/badges/utils.py
@@ -9,11 +9,14 @@ def badges_available_for_course(courseID=None):
 	if courseID is not None:
         	course_details=courseID.split('/')
 		#{org}/{course}/{run} (for example, MITx/6.002x/2012_Fall).	
-		org,course_name,course_run=course_details
-	
 		
-		if org == 'edX' and course_name=='Open_DemoX':
+		org,course_name,course_run=course_details
+		
+		if org == 'edX' and course_name =='Open_DemoX':
 			return a.list('badge',issuer='edX',program='Open_DemoX')
+		
+		if org == 'iuX' and course_name =='cs202x':
+			return a.list('badge',issuer='iuX',program='cs202x')
 		else:
 			return None
 	
