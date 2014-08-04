cfe13a5741a5930e1f9598410e99dfeefd0a3180
karthik <karthikbangera87@gmail.com>
2014-08-04 16:20:54 +0000

    Display the list of earned and unearned badges for a course in progress template

--- lms/djangoapps/badges/utils.py
+++ lms/djangoapps/badges/utils.py
@@ -1,59 +1,59 @@
 #def badges_available_for_course(course):
-    # TODO: really find badges
-    #return []
-import badgekit.api,collections
+# TODO: really find badges
+#return []
 
-
+import badgekit.api
 
 def badges_available_for_course(UserEmail,courseID=None):
-	
+
 	a=badgekit.api.BadgeKitAPI('http://10.0.2.2:5000','hihello',defaults={'system':'badgekit'})
 	if courseID is not None:
-        	course_details=courseID.split('/')
+		course_details=courseID.split('/')
 		#{org}/{course}/{run} (for example, MITx/6.002x/2012_Fall).	
-		
+
 		org,course_name,course_run=course_details
-		
-		if org == 'edX' and course_name =='Open_DemoX':
-			raw_badges= a.list('badge',issuer='edX',program='Open_DemoX')
-	
-	        badges_return={'Unearned':[]}	
-	        for badge in raw_badges['badges']:	
-			badge['is_earned']='False'
-		
-		for badgeid in raw_badges['badges']:
-			if badgeid['id']==7:
-				badgeid['is_earned']='True'
-				badges_return['Earned']=[]
-				badges_return['Earned'].append(badgeid)
-			else:
+		earned_list=[]
+		available_list=[]
+		unearned_list=[]
+		badges_return={'Earned':[],'Unearned':[]}
+		#if org == 'edX' and course_name =='Open_DemoX':
+		badges_available= a.list('badge',system='badgekit',issuer=org,program=course_name)
+		slug_list=[]
+		for badge in badges_available['badges']:
+			 available_list.append(badge['id'])
+			 slug_list.append(badge['slug'])
 			
-				badges_return['Unearned'].append(badgeid)
-		
-		#for badges in raw_badges['badges']:
-			#if badges['is_earned']=='True':
-				#badges_return={'Earned':badges}
+			
+		for item in range(len(slug_list)):
+			raw_badges= a.list('instance',system='badgekit',issuer=org,program=course_name,badge='{}'.format(slug_list[item]))
+			for badge_instances in raw_badges['instances']:
+				if UserEmail == badge_instances['email']:
+					badges_return['Earned'].append(badge_instances['badge'])	
+					earned_list.append(badge_instances['badge']['id'])
+					
+			
+		unearned_list=set(available_list) - set(earned_list)
+				
+		for badge in badges_available['badges']:
+			for item in list(unearned_list):
+				if badge['id'] == item:
+					badges_return['Unearned'].append(badge)	
 			
 		
-			
-		
+				
+						
 		return badges_return
+			#return earned[1][2]['email']	
+			#raw_badges['instances'][0]['badge']['id']
+
+	    	
 		
-		#if raw_badges['badges'][0]['is_earned']=='True':
-			#badges_return={'Earned':raw_badges['badges'][0]}
-		
-		
-		#badges_return['Unearned']=raw_badges['badges'][1]
 		#return badges_return
-		
-		 		
-	 	
-			
 		#if org == 'iuX' and course_name =='cs202x':
 			# a.list('badge',issuer='iuX',program='cs202x')
 		#else:
 			#return None
 	
-
+		
 	else:
 		return courseID
